aafd1ef07d8aeb54f53640de32b7455a3b443b1e
Yutong Huang <yxh589@case.edu>
2018-02-14 20:54:06 -0500

    Implemented varDeclaration and assignment

--- interpreter.scm
+++ interpreter.scm
@@ -1,1 +1,21 @@
 (require "simpleParser.scm")
+
+; defining a function for variable declaration
+(define varDeclaration
+  (lambda (dec)
+    (cond
+      ((null? (cddr dec)) (cdr dec))
+      (else (cdr dec)))))
+
+
+
+; defining a function for assignment
+(define assignment
+  (lambda (asg state)
+    (cond
+      ((null? state) (cdr asg))
+      ((eq? (caar state) (cdar asg)) (append (cdr asg) (cdr state)))
+      ((not (null? (cdr state))) (append (car state) (assign (asg (cdr state)))))
+      (else (error "Variable not declared yet!")))))
+
+; defining a function for return
